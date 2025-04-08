e6964506c4f0a8d3b696604872acd347e2c409b9
Navya-ekkati <navyareddyekkati2@gmail.com>
2025-04-08 15:01:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+str="Navya Reddy"
+console.log(str.length)
+str1 = str.toLowerCase()
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+
+
+if(str.length>6){
+    console.log(str)
+}
+else{
+    console.log("not found")
+}
+console.log(str.slice(6)); 
+let str0 = "Hello world";
+console.log(str0.replace("world", "there")); 
+ a =str.charAt(4)
+console.log(a)
+
+str2 = "Aarvi";
+console.log(str2.charAt(1));
+let sentence = "I love nature";
+console.log(sentence.indexOf("love"));
+console.log(str.includes("Navya")); 
+
